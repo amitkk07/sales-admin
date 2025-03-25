d2a72d8c4629f7d232af5344ca700bec803c8ccc
Amit Kanaujiya <amit.kanaujiya@techplek.in>
2025-03-25 12:36:22 +0530

    new changes

--- src/controllers/stockController.js
+++ src/controllers/stockController.js
@@ -3,11 +3,9 @@ const stockService = require('../services/stockService');
 exports.createStock = async (req, res) => {
   try {
     const { wholesaler_id, retailer_id, stock_amount, date } = req.body;
-    
     if (!wholesaler_id || !retailer_id || !stock_amount || !date) {
       return res.status(400).json({ error: "All fields are required" });
     }
-
     const stockEntry = await stockService.createStockEntry(wholesaler_id, retailer_id, stock_amount, date);
     res.status(201).json(stockEntry);
   } catch (error) {
